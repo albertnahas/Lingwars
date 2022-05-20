f9e22c6178e53cd8662b7302441547ffbc97540e
albertnahas <albert@shortpoint.com>
2022-05-20 11:39:53 +0400

    prettier

--- public/data/index.js
+++ public/data/index.js
@@ -1,29 +1,29 @@
-const fs = require('fs');
+const fs = require("fs")
 
 function getFiles(dir, files_) {
-    files_ = files_ || [];
-    var files = fs.readdirSync(dir);
-    for (var i in files) {
-        var name = dir + '/' + files[i];
-        if (fs.statSync(name).isDirectory()) {
-            getFiles(name, files_);
-        } else {
-            files_.push(name);
-        }
+  files_ = files_ || []
+  var files = fs.readdirSync(dir)
+  for (var i in files) {
+    var name = dir + "/" + files[i]
+    if (fs.statSync(name).isDirectory()) {
+      getFiles(name, files_)
+    } else {
+      files_.push(name)
     }
-    return files_;
+  }
+  return files_
 }
 
-const root = 'audio'
-const output = getFiles(`./${root}`).map(f => f.replace(`./${root}/`, ''))
+const root = "audio"
+const output = getFiles(`./${root}`).map((f) => f.replace(`./${root}/`, ""))
 // stringify JSON Object
-var jsonContent = JSON.stringify(output);
+var jsonContent = JSON.stringify(output)
 
-fs.writeFile("files.json", jsonContent, 'utf8', function (err) {
-    if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
-    }
+fs.writeFile("files.json", jsonContent, "utf8", function (err) {
+  if (err) {
+    console.log("An error occured while writing JSON Object to File.")
+    return console.log(err)
+  }
 
-    console.log("JSON file has been saved.");
-});
+  console.log("JSON file has been saved.")
+})
