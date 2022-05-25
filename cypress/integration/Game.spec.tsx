30a62224fd3dceda8d38ed01d144d8465f075074
albertnahas <albert@shortpoint.com>
2022-05-26 00:21:54 +0400

    test player

--- cypress/integration/Game.spec.tsx
+++ cypress/integration/Game.spec.tsx
@@ -135,7 +135,7 @@ describe("Game starts for multi player - live mode", () => {
         timedScore: 0,
         accuracy: 0,
       }).then(() => {
-        cy.get(`[aria-label="player chip"]`, { timeout: 5000 }).should(
+        cy.get(`[aria-label="player chip"]`, { timeout: 15000 }).should(
           "have.length",
           2
         )
