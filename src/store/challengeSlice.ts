715ea2cca770fbd6ac97673b0c86cff0850268cc
albertnahas <albert@shortpoint.com>
2022-06-05 19:04:43 +0400

    Fix single player

--- src/store/challengeSlice.ts
+++ src/store/challengeSlice.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { Challenge } from "../types/challenge"
+import { Challenge, ChallengeSetup } from "../types/challenge"
 import { ChallengeState, State } from "../types/state"
 
-const initialState: ChallengeState = { value: {} }
+const initialState: ChallengeState = { value: null, setup: null }
 
 export const challengeSlice = createSlice({
   name: "challenge",
@@ -15,11 +15,18 @@ export const challengeSlice = createSlice({
     ) => {
       state.value = action.payload
     },
+    setChallengeSetup: (
+      state: ChallengeState,
+      action: PayloadAction<ChallengeSetup | null>
+    ) => {
+      state.setup = action.payload
+    },
   },
 })
 export const challengeSelector = (state: State) => state.challenge?.value
+export const challengeSetupSelector = (state: State) => state.challenge?.setup
 
 // Action creators are generated for each case reducer function
-export const { setChallenge } = challengeSlice.actions
+export const { setChallenge, setChallengeSetup } = challengeSlice.actions
 
 export default challengeSlice.reducer
