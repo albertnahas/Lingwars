f92485acf5ef11288f38ecdf715a0dddf32dcba1
albertnahas <albert@shortpoint.com>
2022-06-07 12:22:58 +0400

    format messaging

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,30 +1,29 @@
 // Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js")
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js")
 
 // Initialize the Firebase app in the service worker by passing the generated config
 var firebaseConfig = {
-    apiKey: 'AIzaSyDEoVx6711d0M50SVUmXhejuO8shhzFC8I',
-    authDomain: 'photorater-c5c6a.firebaseapp.com',
-    projectId: 'photorater-c5c6a',
-    storageBucket: 'photorater-c5c6a.appspot.com',
-    messagingSenderId: '1564450420',
-  appId: "1:1564450420:web:2b0bf08fd2f76c8e66bb21",
-    measurementId: 'G-6232FMVPW3'
-};
+  apiKey: "AIzaSyBtVXwmmTFsT6ohl06zew8k-6c_8CmV7Zg",
+  authDomain: "lingwars-2ee8b.firebaseapp.com",
+  databaseURL: "https://lingwars-2ee8b-default-rtdb.firebaseio.com",
+  projectId: "lingwars-2ee8b",
+  storageBucket: "lingwars-2ee8b.appspot.com",
+  messagingSenderId: "1032612550540",
+  appId: "1:1032612550540:web:a07dc1a13b9f239f6d28a9",
+  measurementId: "G-8MDJN04EXZ",
+}
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
 // Retrieve firebase messaging
-const messaging = firebase.messaging();
+const messaging = firebase.messaging()
 
 messaging.onBackgroundMessage(function (payload) {
-
-    const notificationTitle = payload.notification.title;
+  const notificationTitle = payload.notification.title
   const notificationOptions = {
-        body: payload.notification.body,
-    };
+    body: payload.notification.body,
+  }
 
-    self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
+  self.registration.showNotification(notificationTitle, notificationOptions)
+})
